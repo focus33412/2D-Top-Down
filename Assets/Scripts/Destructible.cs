894eee50090d6cfbdb941c6428c6f280cd4aa5f1
FOCUS <150962625+focus33412@users.noreply.github.com>
2025-05-27 20:14:05 +0300

    destroy transparent

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destoyVFX;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.GetComponent<DamageSource>())
+        {
+            Instantiate(destoyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
